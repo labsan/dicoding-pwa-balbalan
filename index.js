a02c9fbba15a743bb680b8765c4e75c22d4a967e
labsan <59597107+labsan@users.noreply.github.com>
2020-11-27 14:33:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,6 @@ import App from './scripts/view/App.js';
 import registerSW from './scripts/utils/register-sw.js';
 import registerNotif from './scripts/utils/register-notif.js';
 
-// Improve Scrolling Performance
-document.addEventListener('touchstart', () => {}, {
-    passive: true
-});
-
-document.addEventListener('touchmove', event => {
-    console.log(event.defaultPrevented);
-    event.preventDefault();
-    console.log(event.defaultPrevented);
-}, {
-    passive: true
-});
-
 const app = new App({
     button: document.getElementById('nav-mobile'),
     drawer: document.querySelectorAll('ul a'),
