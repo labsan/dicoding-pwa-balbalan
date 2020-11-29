19a5000126b2ff6f7cca11870c664ded1d85dade
labsan <59597107+labsan@users.noreply.github.com>
2020-11-30 01:40:05 +0700

    Update serviceworker.js
    update precacheAndRoute() function and add new workbox caching

--- serviceworker.js
+++ serviceworker.js
@@ -4,145 +4,58 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 if (workbox) {
     console.log('Workbox berhasil dimuat!.');
 
-    // Precaching App Shell
-    workbox.precaching.precacheAndRoute([{
-        url: '/',
-        revision: '1'
-    }, {
-        url: 'manifest.json',
-        revision: '1'
-    }, {
-        url: 'index.html',
-        revision: '1'
-    }, {
-        url: 'index.js',
-        revision: '1'
-    }, {
-        url: 'push.js',
-        revision: '1'
-    }, {
-        url: 'styles/libs/materialize.min.css',
-        revision: '1'
-    }, {
-        url: 'styles/custom.css',
-        revision: '1'
-    }, {
-        url: 'images/icon-mola-tv.png',
-        revision: '1'
-    }, {
-        url: 'images/maskable_icon-16x16-v2.png',
-        revision: '1'
-    }, {
-        url: 'images/maskable_icon-32x32-v2.png',
-        revision: '1'
-    }, {
-        url: 'images/maskable_icon-96x96-v2.png',
-        revision: '1'
-    }, {
-        url: 'images/maskable_icon-192x192-v2.png',
-        revision: '1'
-    }, {
-        url: 'images/maskable_icon_apple-192x192-v2.png',
-        revision: '1'
-    }, {
-        url: 'images/maskable_icon-512x512-v2.png',
-        revision: '1'
-    }, {
-        url: 'scripts/libs/materialize.min.js',
-        revision: '1'
-    }, {
-        url: 'scripts/libs/idb.min.js',
-        revision: '1'
-    }, {
-        url: 'scripts/data/data-klasemen.js',
-        revision: '1'
-    }, {
-        url: 'scripts/data/data-klub.js',
-        revision: '1'
-    }, {
-        url: 'scripts/data/data-favorit-klub.js',
-        revision: '1'
-    }, {
-        url: 'scripts/routes/route.js',
-        revision: '1'
-    }, {
-        url: 'scripts/routes/parse-url.js',
-        revision: '1'
-    }, {
-        url: 'scripts/utils/initiator-drawer.js',
-        revision: '1'
-    }, {
-        url: 'scripts/utils/register-notif.js',
-        revision: '1'
-    }, {
-        url: 'scripts/utils/register-sw.js',
-        revision: '1'
-    }, {
-        url: 'scripts/utils/urlBase64ToUint8Array.js',
-        revision: '1'
-    }, {
-        url: 'scripts/view/App.js',
-        revision: '1'
-    }, {
-        url: 'scripts/view/pages/klasemen.js',
-        revision: '1'
-    }, {
-        url: 'scripts/view/pages/klub.js',
-        revision: '1'
-    }, {
-        url: 'scripts/view/pages/favorit-klub.js',
-        revision: '1'
-    }, {
-        url: 'scripts/view/pages/info-klub.js',
-        revision: '1'
-    }]);
+ // Precaching App Shell
+    workbox.precaching.precacheAndRoute([
+        { url: '/', revision: '1' }, 
+        { url: 'index.html', revision: '1' },
+        { url: 'manifest.json', revision: '1' },
+        { url: 'index.js', revision: '1' },
+        { url: 'serviceworker.js', revision: '1' },
+        { url: 'push.js', revision: '1' },
 
-    // Routing styles folder
-    workbox.routing.registerRoute(
-        new RegExp('styles/'),
-        workbox.strategies.cacheFirst({
-            cacheName: 'local-styles'
-        })
-    );
+        { url: 'styles/libs/materialize.min.css', revision: '1' },
+        { url: 'styles/custom.css', revision: '1' },
 
-    // Routing images folder
-    workbox.routing.registerRoute(
-        new RegExp('images/'),
-        workbox.strategies.cacheFirst({
-            cacheName: 'local-images'
-        })
-    );
+        { url: 'scripts/libs/materialize.min.js', revision: '1' },
+        { url: 'scripts/libs/idb.min.js', revision: '1' },
+        
+        { url: 'scripts/utils/register-notif.js', revision: '1' },
+        { url: 'scripts/utils/register-sw.js', revision: '1' },
+        { url: 'scripts/utils/initiator-drawer.js', revision: '1' },
+        { url: 'scripts/utils/urlBase64ToUint8Array.js', revision: '1' },
 
-    // Routing pages folder
-    workbox.routing.registerRoute(
-        new RegExp('scripts/view/pages/'),
-        workbox.strategies.cacheFirst({
-            cacheName: 'local-pages'
-        })
-    );
+        { url: 'scripts/routes/route.js', revision: '1' },
+        { url: 'scripts/routes/parse-url.js', revision: '1' },
+        
+        { url: 'scripts/view/App.js', revision: '1' },
+        { url: 'scripts/view/pages/favorit-klub.js', revision: '1' },
+        { url: 'scripts/view/pages/info-klub.js', revision: '1' },
+        { url: 'scripts/view/pages/klasemen.js', revision: '1' },
+        { url: 'scripts/view/pages/klub.js', revision: '1' },
+        { url: 'scripts/view/pages/pertandingan.js', revision: '1' },
+        
+        { url: 'scripts/data/data-favorit-klub.js', revision: '1' },
+        { url: 'scripts/data/data-klasemen.js', revision: '1' },
+        { url: 'scripts/data/data-klub.js', revision: '1' },
+        { url: 'scripts/data/data-pertandingan.js', revision: '1' },
 
-    // Route for setting image files storage [saved cache]
-    workbox.routing.registerRoute(
-        /\.(?:png|gif|jpg|jpeg|svg)$/,
-        workbox.strategies.cacheFirst({
-            cacheName: 'storage-images',
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 60,
-                    maxAgeSeconds: 30 * 24 * 60 * 60
-                }),
-            ],
-        }),
-    );
+        { url: 'images/maskable_icon-16x16-v2.png', revision: '1' },
+        { url: 'images/maskable_icon-32x32-v2.png', revision: '1' },
+        { url: 'images/maskable_icon-96x96-v2.png', revision: '1' },
+        { url: 'images/maskable_icon-192x192-v2.png', revision: '1' },
+        { url: 'images/maskable_icon_apple-192x192-v2.png', revision: '1' },
+        { url: 'images/maskable_icon-512x512-v2.png', revision: '1' },
+        { url: 'images/icon-mola-tv.png', revision: '1' }
+    ]);
 
-    // Route for setting API google fonts data storage [saved cache]
+    // Management caching API google fonts data storage in browser
     workbox.routing.registerRoute(
         /^https:\/\/fonts\.gstatic\.com/,
-        workbox.strategies.cacheFirst({
-            cacheName: 'storage-api-google-fonts',
+        workbox.strategies.staleWhileRevalidate({
+            cacheName: 'caching-api-google-fonts',
             plugins: [
                 new workbox.cacheableResponse.Plugin({
-                    statuses: [0, 200]
+                    statuses: [200]
                 }),
                 new workbox.expiration.Plugin({
                     maxAgeSeconds: 60 * 60 * 24 * 365,
@@ -152,51 +65,37 @@ if (workbox) {
         })
     );
 
-    // Caching styles folder
-    workbox.routing.registerRoute(
-        new RegExp('styles/'),
-        workbox.strategies.staleWhileRevalidate({
-            cacheName: 'caching-style-files'
-        })
-    );
-
-    // Caching images folder
-    workbox.routing.registerRoute(
-        new RegExp('images/'),
-        workbox.strategies.staleWhileRevalidate({
-            cacheName: 'caching-image-files'
-        })
-    );
-
-    // Caching pages folder
-    workbox.routing.registerRoute(
-        new RegExp('scripts/view/pages/'),
-        workbox.strategies.staleWhileRevalidate({
-            cacheName: 'caching-page-files'
-        })
-    );
-
-    // Caching API google fonts
-    workbox.routing.registerRoute(
-        /^https:\/\/fonts\.gstatic\.com/,
-        workbox.strategies.staleWhileRevalidate({
-            cacheName: 'caching-api-google-fonts'
-        })
-    );
-
-    // Caching API football data
+    // Management caching API football data storage in browser
     workbox.routing.registerRoute(
         new RegExp('https://api.football-data.org/'),
         workbox.strategies.staleWhileRevalidate({
-            cacheName: 'caching-api-football-data'
+            cacheName: 'caching-api-football-data',
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [200]
+                }),
+                new workbox.expiration.Plugin({
+                    maxAgeSeconds: 60 * 60 * 24 * 365,
+                    maxEntries: 30
+                }),
+            ]
         })
     );
 
-    // Caching API image team football
+    // Management caching API image team data storage in browser
     workbox.routing.registerRoute(
         new RegExp('https://crests.football-data.org/'),
         workbox.strategies.staleWhileRevalidate({
-            cacheName: 'caching-api-image-team-football'
+            cacheName: 'caching-api-image-team-football',
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [200]
+                }),
+                new workbox.expiration.Plugin({
+                    maxAgeSeconds: 60 * 60 * 24 * 365,
+                    maxEntries: 30
+                }),
+            ]
         })
     );
 
